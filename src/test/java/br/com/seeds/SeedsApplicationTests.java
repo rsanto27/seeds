7fd2599c57260e1ae957b51c6244450f40ef9085
Rodrigo Costa <rodrigocostasantos07@gmail.com>
2024-01-20 12:24:25 -0300

    first commit

---
+++ src/test/java/br/com/seeds/SeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

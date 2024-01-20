4c3f580228c8953a18a2f311ce8ad9a17c20d958
Rodrigo Costa <rodrigocostasantos07@gmail.com>
2024-01-20 12:53:13 -0300

    add application module

--- seed-application/src/main/java/br/com/seeds/Application.java
+++ seed-application/src/main/java/br/com/seeds/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeedsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeedsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
